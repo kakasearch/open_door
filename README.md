444f37ac0aece9f4dd622138ab9b0010bdb4b9b0
kaka <62104649+kakasearch@users.noreply.github.com>
2021-11-19 23:36:33 +0800

    完成咯

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # open_door
  利用 nodemcu 实现无线开门
+# 功能
+- 支持局域网控制，访问`http://{esp8266的IP}?gpio=on`时开门 
+- 支持小爱同学控制
+- 支持小度语音助手
+- 可通过小度语音助手的app抓包实现外网控制开门。如果不抓包，就需要内网穿透才能在外网控制
+- 支持刷卡开门
+
+# 需要的元件
+1. nodemcu 或者arduino+esp8266
+2. rc522模块
+3. 舵机
+4. 导线、电源（舵机需要至少2A的电源才能动起来）
+5. led（非必要）、蜂鸣器（非必要）
+# 连线
+- rc522与nodemcu连线参考：https://blog.csdn.net/qq_31878883/article/details/88971935
+- 舵机 data线连GPIO15(nodemcu的d8),vcc连vin或者连外接电源，目的是提供5V和2A的电流
+- led灯珠连在GPIO10上，开门后会亮起作为已开门的提醒
+# 需要修改的地方
+```c++
+#define blinksk "blink的密钥" //12行
+#define STASSID "wifi名字" //13行
+#define STAPSK  "wifi密码"  //14行
+
+byte data[5][4] = { 0x19,0x04,0x24,0xBE, //在代码28行。管理员的16进制卡号，可通过rc522库的示例代码dumpinfo获得
+                    0x01,0xC1,0x1E,0x1C, //每张卡都是4字节
+};
+```
+# 扩展
+- 通过对小度app抓包，可模拟小度的指令，作为API提供给其他程序调用。如qq机器人、手机app、tasker自动化程序等等。。。
+-API示例
+```python
+import requests
+def openTheDoor():
+    api = 'https://xiaodu.baidu.com/saiya/smarthome/directivesend?from=h5_control'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 11; RMX3161 Build/RKQ1.201217.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/92.0.4515.159 Mobile Safari/537.36 FromApp/XiaoDuApp oneapp/4.1.0.1 sdk/0.7.0',
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Accept': '*/*',
+        'Origin': 'https://xiaodu.baidu.com',
+        'X-Requested-With': 'com.baidu.duer.superapp',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://xiaodu.baidu.com/saiya/smarthome/index.html',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Cookie': 'your cookie!!!!!!!!!!!!!!!',
+    }
+    data = '''{"header":{"namespace":"DuerOS.ConnectedHome.Control","name":"TurnOnRequest","payloadVersion":1},"payload":{"appliance":{"applianceId":["your ID!!!请自行抓取！！！！！！！！！！"]}}}'''
+    request.post(api,headers=headers,data=data)
+
+```
--- door/door.ino
+++ door/door.ino
@@ -5,21 +5,29 @@
 #define BLINKER_WITHOUT_SSL
 #include <Blinker.h>
 #include <ESP8266WiFi.h>
-#define blinksk "53edf7d7243f"
-#define STASSID "HiWiFi_5C0B98"
-#define STAPSK  "gzwl6009"
+#include <SPI.h>
+#include <MFRC522.h>
+#define RST_PIN         5           // 配置针脚
+#define SS_PIN          4
+#define blinksk "blink的密钥"
+#define STASSID "wifi名字"
+#define STAPSK  "wifi密码"
 
 
 
 #define maxd 180
 #define mind 5
-#define led 5
-//D1
-#define buzzerPin 12 
-//D6
-#define ServoPin 14 //d5
-WiFiServer server(35614);//开启板子的port 80
-bool oState = 0;
+#define led 10         //s3
+#define buzzerPin 2 //D3 喇叭弃用
+#define ServoPin 15 //d8
+WiFiServer server(80);//开启板子的port 80
+int oState = 0;
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // 创建新的RFID实例
+MFRC522::MIFARE_Key key;
+byte data[5][4] = { 0x19,0x04,0x24,0xBE, //管理员的16进制卡号，数组里的uid将放行
+                    0x01,0xC1,0x1E,0x1C,                 
+};
 void ServoControl(int servoAngle)
 {
   Serial.println("servo");
@@ -164,16 +172,58 @@ void server_run(){
   client.print(s);
   // 比对收到的讯息，确定执行什么操作
   if (req.indexOf("?gpio=on") != -1){
-    Serial.println("open door");//打印出收到的讯息
     oState = 3;
   }else {
     Serial.println("invalid request");
   }
   client.flush();//刷新
 }
-
+int check_uid(byte *buffer) {
+  Serial.print("uid:");
+  for (byte i = 0; i <4; i++) {
+    Serial.print(buffer[i], HEX);
+  }
+  Serial.println();
+  for (byte i = 0; i <5; i++) {
+    byte r_num = 0;
+    for (byte j = 0; j <4; j++) {
+      if (buffer[j] == data[i][j]){
+        r_num ++;
+      }
+    }
+    if(r_num == 4){
+      return 1;
+      }
+  }
+  return 0;
+}
+void rfc_run(){
+  
+  // 寻找新卡
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    //Serial.println("没有找到卡");
+    return;
+  }
+  // 选择一张卡
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    Serial.println("没有卡可选");
+    return;
+  }
+  if(check_uid(mfrc522.uid.uidByte)){
+     Serial.println("card yes");
+      oState = 4;
+    }else{
+      Serial.println("card error");
+      oState = 0;
+      }
+  //停止 PICC
+ // mfrc522.PICC_HaltA();
+  //停止加密PCD
+//  mfrc522.PCD_StopCrypto1();
+  //return;
+  }
 void setup() {
-  pinMode(buzzerPin,OUTPUT);
+  //pinMode(buzzerPin,OUTPUT);
   pinMode(ServoPin,OUTPUT);
   pinMode(led,OUTPUT);
   Serial.begin(115200);//开启端口，速度为115200
@@ -188,9 +238,6 @@ void setup() {
 
   BlinkerDuerOS.attachPowerState(DuerOSPowerState);
   BlinkerDuerOS.attachQuery(duerQuery);
-
-
-
  while (WiFi.status() != WL_CONNECTED) {//连线成功后停止跳点
     delay(500);
     Serial.print(".");
@@ -198,10 +245,10 @@ void setup() {
   Serial.println("\nWiFi connected");
   server.begin();
   Serial.println("Server started");
-  
   Serial.println(WiFi.localIP());
   //open_door();
-  Serial.println("servo test ok");
+  SPI.begin();        // SPI开始
+  mfrc522.PCD_Init(); // Init MFRC522 card
   pinMode(2,OUTPUT);
   digitalWrite(2,HIGH);
 }
@@ -210,6 +257,7 @@ void loop() {
   // Check if a client has connected
   Blinker.run();
   server_run();
+  rfc_run();
   if(oState == 1){
     open_door(15000);
   }
@@ -219,5 +267,7 @@ void loop() {
   if(oState == 3){
     open_door(15000);
   }
-
+  if(oState == 4){
+    open_door(5000);
+  }
 }
